105d13209329b9883d1668fe5e227ad1e9caa1c7
nohhow <xksrma97@gmail.com>
2022-03-29 18:01:30 +0900

    add .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL : "https://api.themoviedb.org/3",
     params : {
-        api_key: "e14409facdc2c58de314693efe970e32",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR",
     },
 });
